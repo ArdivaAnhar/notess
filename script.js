5184b3764efa3d2ed87e44996ef6b7b2db511ac6
anata_mee <108978057+ArdivaAnhar@users.noreply.github.com>
2023-11-28 22:26:16 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,18 @@
-const minutes = new Date().getMinutes();
-const hours = new Date().getHours();
-const day = new Date().getDate();
-const month = new Date().getMonth();
-const year = new Date().getFullYear();
+document.addEventListener("DOMContentLoaded", function () {
+  const currentDate = new Date();
+  const minutes = currentDate.getMinutes();
+  const hours = currentDate.getHours();
+  const day = currentDate.getDate();
+  const month = currentDate.getMonth() + 1;
+  const year = currentDate.getFullYear();
+  const ampm = hours >= 12 ? "PM" : "AM";
+  const formattedHours = hours % 12 || 12;
+  const formattedTime = `${formattedHours}:${minutes} ${ampm}`;
+  const formattedDate = `${day}/${month}/${year}`;
 
-document.getElementById(
-  "date_1"
-).innerHTML = `${hours}:${minutes} ${day}/${month}/${year}`;
+  const date = document.getElementsByClassName("time");
 
-document.getElementById(
-  "date_2"
-).innerHTML = `${hours}:${minutes} ${day}/${month}/${year}`;
-
-document.getElementById(
-  "date_3"
-).innerHTML = `${hours}:${minutes} ${day}/${month}/${year}`;
+  for (let i = 0; i < date.length; i++) {
+    date[i].innerHTML = `${formattedTime} ${formattedDate}`;
+  }
+});
